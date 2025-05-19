e93125ed81c45983b87e92e031f633a172be8f49
Gabrielle Soares Teixeira - 422221763 <422221763@ulife.com.br>
2025-05-19 20:38:41 -0300

    mudei uns trens

--- Front-End/site/src/components/Footer.tsx
+++ Front-End/site/src/components/Footer.tsx
@@ -5,12 +5,7 @@ const Footer = () => {
     <footer className="super-footer">
       <div className="footer-container">
        
-        
-        <nav className="footer-nav">
-          <Link to="/">Home</Link>
-          <Link to="/about">Sobre</Link>  {/* Navega para /about */}
-          <Link to="/contact">Contato</Link>
-        </nav>
+
          <p>© 2025 MeuSite. Todos os direitos reservados.</p>
       </div>
     </footer>
