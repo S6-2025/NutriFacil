019672cb84b69f14b215eeadc92027dd28b46595
gabriellesote <gabrielle.sote@gmail.com>
2025-05-27 18:01:21 -0300

    criando hooks, types, e services para tentar consumir a api

---
+++ Front-End/site/src/hooks/useRegisterForm.ts
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import type { RegisterRequestDTO } from '../types/RegisterRequestDTO';
+import { registerUser } from '../services/authService';
+
+export function useRegisterForm() {
+  const [form, setForm] = useState<RegisterRequestDTO>({
+    username: '',
+    password: '',
+    fullname: '',
+    email: '',
+    gender: '',
+    age: 0,
+    weight: 0,
+    height: 0,
+  });
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    setForm((prev) => ({
+      ...prev,
+      [name]: ['age', 'weight', 'height'].includes(name)
+        ? Number(value)
+        : value,
+    }));
+  }
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    try {
+      await registerUser(form);
+      alert('Usuário registrado com sucesso.');
+    } catch (err) {
+      alert('Erro ao registrar usuário.');
+    }
+  }
+
+  return { form, handleChange, handleSubmit };
+}
--- Front-End/site/src/pages/Register.tsx
+++ Front-End/site/src/pages/Register.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+ 
 const Register: React.FC = () => {
+
+
+  
   return (
     <main className="super-container" id="super-container-form">
       <section className="super-form-register">
