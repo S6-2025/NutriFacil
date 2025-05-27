784bb8f15d9993ad45293ccd92be1b9cedb944c0
gabriellesote <gabrielle.sote@gmail.com>
2025-05-27 18:01:21 -0300

    criando hooks, types, e services para tentar consumir a api

---
+++ Front-End/site/src/types/RegisterRequestDTO.ts
@@ -0,0 +1,10 @@
+export interface RegisterRequestDTO {
+  username: string;
+  password: string;
+  fullname: string;
+  email: string;
+  gender: string;
+  age: number;
+  weight: number;
+  height: number;
+}
