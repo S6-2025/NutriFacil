2eee601b7a4c451041ff14161bcdf189e1bd0d67
gabriellesote <gabrielle.sote@gmail.com>
2025-05-28 20:42:57 -0300

    subindo o escopo

--- Front-End/site/src/pages/Result.tsx
+++ Front-End/site/src/pages/Result.tsx
@@ -18,29 +18,15 @@ const Result: React.FC = () => {
             <tbody>
               <tr>
                 <th>Segunda</th>
-                <td>Maçã 80kcal
-                    Banana  90kcal
-                    Morango  50kcal
-                </td>
-                <td>Maçã 80kcal
-                    Banana  90kcal
-                    Morango  50kcal</td>
-                <td>Maçã 80kcal
-                    Banana  90kcal
-                    Morango  50kcal</td>
+                <td> 300kcal</td>
+                <td>300kcal</td>
+                <td>300kcal</td>
               </tr>
               <tr>
                 <th>Terça</th>
-                <td>Maçã 80kcal
-                    Banana  90kcal
-                    Morango  50kcal
-                     </td>
-                <td> Maçã 80kcal
-                    Banana  90kcal
-                    Morango  50kcal</td>
-                <td> Maçã 80kcal
-                    Banana  90kcal
-                    Morango  50kcal</td>
+                <td>Maçã 80kcal Banana 90kcal Morango 50kcal</td>
+                <td> Maçã 80kcal Banana 90kcal Morango 50kcal</td>
+                <td> Maçã 80kcal Banana 90kcal Morango 50kcal</td>
               </tr>
               <tr>
                 <th>Quarta</th>
@@ -76,10 +62,18 @@ const Result: React.FC = () => {
                 <th className="border-left">Total Kcal</th>
                 <td>1000kcal</td>
                 <td>1000kcal</td>
-                <td className="border-right" >1000kcal</td>
+                <td className="border-right">1000kcal</td>
               </tr>
             </tbody>
           </table>
+        </div>
+        
+
+        <div>
+          <h2> Sugestão para alimentos</h2>
+          <h3>Café da manhã</h3>
+           <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, officia velit numquam quasi aliquid voluptas ipsum esse non vitae blanditiis, possimus cumque, dolore suscipit ratione! Tempora, in. Eum, perferendis vero.</p>
+           
         </div>
 
         <div>
