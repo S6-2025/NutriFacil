7c4bfe7ffd2905721462db1d11cbf7e6dc7d64d9
Vinícius Madureira Godinho de Oliveira <vinimagod@gmail.com>
2025-05-29 14:13:08 -0300

    corrigindo linhas duplicadas

--- Front-End/site/src/services/registerUser.ts
+++ Front-End/site/src/services/registerUser.ts
@@ -1,9 +1,5 @@
 export async function registerUser(data: any): Promise<void> {
-<<<<<<< HEAD
   const response = await fetch("https://6b89-2804-1b3-c483-ecf8-f44f-e396-96f3-31f5.ngrok-free.app/auth/register", {
-=======
-  const response = await fetch("http://localhost:3000/register", {
->>>>>>> 504d1b975a1a208df8a115964229f9da43f95d88
     method: "POST",
     headers: {
       "Content-Type": "application/json",
