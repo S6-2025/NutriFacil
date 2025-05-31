3298824f0fe72f8ad8099a609fd5d12187e28624
gabriellesote <gabrielle.sote@gmail.com>
2025-05-31 14:35:10 -0300

    finalizando o result

--- Front-End/site/src/pages/Result.tsx
+++ Front-End/site/src/pages/Result.tsx
@@ -32,12 +32,13 @@ const Result: React.FC = () => {
                 </svg>
                 <span> Mediterranea</span>
               </div>
-              
+
             </div>
 
           </div>
         </div>
       </div>
+
       <section className="section-divider">
         <svg
           id="wave-profile"
@@ -46,13 +47,14 @@ const Result: React.FC = () => {
         >
           <path
             fill="#008053"
-            fill-opacity="1"
+            fillOpacity="1"
             d="M0,0L60,5.3C120,11,240,21,360,26.7C480,32,600,32,720,42.7C840,53,960,75,1080,80C1200,85,1320,75,1380,69.3L1440,64L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
           ></path>
         </svg>
       </section>
 
       <div className="results-container">
+
         <div className="tmb">
           <h2>Taxa Metábolica Basal</h2>
           <h3 className="h3-with-blob">43.9</h3>
@@ -87,6 +89,14 @@ const Result: React.FC = () => {
             </span>
           </IgrExpansionPanel>
         </div>
+
+
+          <div className="tmb">
+          <h2>Consumo diário de água</h2>
+          <h3 className="h3-with-blob" id="water">2.5L</h3>
+         
+        </div>
+
       </div>
 
       <div className="kcal-container">
@@ -118,7 +128,7 @@ const Result: React.FC = () => {
       <div className="meals-container">
         <h2> Sugestão para alimentos</h2>
         <IgrExpansionPanel>
-          <h1 slot="title">Café da Manhã</h1>
+          <h1 slot="title">Proteínas</h1>
           <h3 slot="subtitle"></h3>
           <span>
             <table className="table-kcals">
@@ -139,29 +149,184 @@ const Result: React.FC = () => {
                   <td>300 kcal</td>
                   <td>1000 kcal</td>
                 </tr>
+                 <tr>
+                  <th>Calorias</th>
+                  <td>Aveia</td>
+                  <td>300 kcal</td>
+                  <td>300 kcal</td>
+                  <td>1000 kcal</td>
+                </tr>
+                 <tr>
+                  <th>Calorias</th>
+                  <td>Iorgute</td>
+                  <td>300 kcal</td>
+                  <td>300 kcal</td>
+                  <td>1000 kcal</td>
+                </tr>
+                 <tr>
+                  <th>Calorias</th>
+                  <td>Maçã</td>
+                  <td>300 kcal</td>
+                  <td>300 kcal</td>
+                  <td>1000 kcal</td>
+                </tr>
+              </tbody>
+            </table>
+          </span>
+        </IgrExpansionPanel>
+          <br />
+        <IgrExpansionPanel>
+          <h1 slot="title">Legumes</h1>
+          <h3 slot="subtitle"></h3>
+          <span>
+            <table className="table-kcals">
+              <thead>
+                <tr>
+                  <th> </th>
+                  <th>Alimento </th>
+                  <th>100g</th>
+                  <th>80g</th>
+                  <th>50g</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Calorias</th>
+                  <td>Banana</td>
+                  <td>300 kcal</td>
+                  <td>300 kcal</td>
+                  <td>1000 kcal</td>
+                </tr>
+                 <tr>
+                  <th>Calorias</th>
+                  <td>Aveia</td>
+                  <td>300 kcal</td>
+                  <td>300 kcal</td>
+                  <td>1000 kcal</td>
+                </tr>
+                 <tr>
+                  <th>Calorias</th>
+                  <td>Iorgute</td>
+                  <td>300 kcal</td>
+                  <td>300 kcal</td>
+                  <td>1000 kcal</td>
+                </tr>
+                 <tr>
+                  <th>Calorias</th>
+                  <td>Maçã</td>
+                  <td>300 kcal</td>
+                  <td>300 kcal</td>
+                  <td>1000 kcal</td>
+                </tr>
+              </tbody>
+            </table>
+          </span>
+        </IgrExpansionPanel>
+          <br />
+        <IgrExpansionPanel>
+          <h1 slot="title">Verduras</h1>
+          <h3 slot="subtitle"></h3>
+          <span>
+            <table className="table-kcals">
+              <thead>
+                <tr>
+                  <th> </th>
+                  <th>Alimento </th>
+                  <th>100g</th>
+                  <th>80g</th>
+                  <th>50g</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Calorias</th>
+                  <td>Banana</td>
+                  <td>300 kcal</td>
+                  <td>300 kcal</td>
+                  <td>1000 kcal</td>
+                </tr>
+                 <tr>
+                  <th>Calorias</th>
+                  <td>Aveia</td>
+                  <td>300 kcal</td>
+                  <td>300 kcal</td>
+                  <td>1000 kcal</td>
+                </tr>
+                 <tr>
+                  <th>Calorias</th>
+                  <td>Iorgute</td>
+                  <td>300 kcal</td>
+                  <td>300 kcal</td>
+                  <td>1000 kcal</td>
+                </tr>
+                 <tr>
+                  <th>Calorias</th>
+                  <td>Maçã</td>
+                  <td>300 kcal</td>
+                  <td>300 kcal</td>
+                  <td>1000 kcal</td>
+                </tr>
               </tbody>
             </table>
           </span>
         </IgrExpansionPanel>
         <br />
         <IgrExpansionPanel>
-          <h1 slot="title">Jantar</h1>
+          <h1 slot="title">Carboidratos</h1>
           <h3 slot="subtitle"></h3>
           <span>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Architecto quaerat beatae vel itaque accusamus quis error rem
-              blanditiis eius delectus quos, atque quasi ab dolor molestias
-              corrupti impedit fuga fugit!
-            </p>
+            <table className="table-kcals">
+              <thead>
+                <tr>
+                  <th> </th>
+                  <th>Alimento </th>
+                  <th>100g</th>
+                  <th>80g</th>
+                  <th>50g</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Calorias</th>
+                  <td>Banana</td>
+                  <td>300 kcal</td>
+                  <td>300 kcal</td>
+                  <td>1000 kcal</td>
+                </tr>
+                 <tr>
+                  <th>Calorias</th>
+                  <td>Aveia</td>
+                  <td>300 kcal</td>
+                  <td>300 kcal</td>
+                  <td>1000 kcal</td>
+                </tr>
+                 <tr>
+                  <th>Calorias</th>
+                  <td>Iorgute</td>
+                  <td>300 kcal</td>
+                  <td>300 kcal</td>
+                  <td>1000 kcal</td>
+                </tr>
+                 <tr>
+                  <th>Calorias</th>
+                  <td>Maçã</td>
+                  <td>300 kcal</td>
+                  <td>300 kcal</td>
+                  <td>1000 kcal</td>
+                </tr>
+              </tbody>
+            </table>
           </span>
         </IgrExpansionPanel>
+        <br />
       </div>
 
-      <div className="data-container">
+      {/* <div className="data-container">
         <canvas id="myChart"></canvas>
-      </div>
+      </div> */}
     </div>
+
+    
   );
 };
 
