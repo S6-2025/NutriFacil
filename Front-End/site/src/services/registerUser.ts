981aff10d604cb36dfb4698b861d05751a9230da
gabriellesote <gabrielle.sote@gmail.com>
2025-06-04 20:39:55 -0300

    nova porta

--- Front-End/site/src/services/registerUser.ts
+++ Front-End/site/src/services/registerUser.ts
@@ -1,5 +1,5 @@
 export async function registerUser(data: any): Promise<void> {
-  const response = await fetch("https://6b89-2804-1b3-c483-ecf8-f44f-e396-96f3-31f5.ngrok-free.app/auth/register", {
+  const response = await fetch("http://localhost:3030/auth/register", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
